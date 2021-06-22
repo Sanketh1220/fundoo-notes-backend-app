3677b664bcadd3ec5fbe810fa4733c2e865a0941
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-06-22 11:45:32 +0530

    docs: added docs and commenting to redis.js file

--- app/middleware/redis.js
+++ app/middleware/redis.js
@@ -1,3 +1,19 @@
+/*********************************************************************
+ * Execution    : 1. Default node with npm   cmd> node server.js
+ *                2. If nodemon installed    cmd> npm start
+ *
+ * Purpose      : To send data to user using redis DB
+ *
+ * @description
+ *
+ * @file        : middleware/redis.js
+ * @overview    : helps to provide data to user
+ * @module      : this is necessary to access the data in the database.
+ * @author      : Sanketh Chigurupalli <sanketh.chigurupalli@gmail.com>
+ * @version     : - - -
+ * @since       : 22-06-2021
+ *********************************************************************/
+
 const redis = require('redis');
 const client = redis.createClient(process.env.REDIS_PORT);
 
